a1426beddcb888f5ef229e2cbd4018d3a7890201
kaueleao <kaue44@yahoo.com.br>
2022-10-14 12:13:26 -0300

    Initial Commit

---
+++ Meu_Teste/Meu_TesteApp.swift
@@ -0,0 +1,17 @@
+//
+//  Meu_TesteApp.swift
+//  Meu_Teste
+//
+//  Created by Kauê Leão on 14/10/22.
+//
+
+import SwiftUI
+
+@main
+struct Meu_TesteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
